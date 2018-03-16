2d0f037feee21434f766f7b23f1cf871b00c57fa
Fitz354 <lozovsky.alx@yandex.ru>
2018-03-16 20:10:01 +0300

    add graphic

--- js/main.js
+++ js/main.js
@@ -1,31 +1,58 @@
 import { findPitch } from 'pitchy';
 import findNote from './findNote';
 
-const noteElement = document.querySelector('.tuner-note');
+const noteElement = document.querySelector('.note');
 const pointerElement = document.querySelector('.pointer');
+const lightsElements = [...document.querySelectorAll('.light')];
+
+const renderLights = offsetClass =>
+  lightsElements.forEach(item => item.style.opacity = (item.classList.contains(offsetClass) ? 1 : 0.3));
 
 const renderNote = ({ note, offset, octave }) => {
-  pointerElement.style.top = `${100 - ( offset + 50)}%`;
+  const pointerAngle = offset * 90 / 50;
+  pointerElement.style.transform =
+    `translate(-50%, -50%) rotate(${pointerAngle}deg)`;
   noteElement.innerHTML = note + octave;
+
+  if (offset => -5 && offset <= 5 ) {
+    renderLights('light-normal');
+  } else if (offset > 5) {
+    renderLights('light-dies');
+  } else {
+    renderLights('light-bemole');
+  }
+};
+
+const drawScale = () => {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'green';
+  ctx.beginPath();
+  ctx.arc(150, 150, 150, 0, Math.PI, true);  // Mouth (clockwise)
+  ctx.stroke();
 };
 
 navigator.mediaDevices.getUserMedia({ audio: true })
   .then(stream => {
     const context = new AudioContext();
-    const analyser = context.createAnalyser();
     const source = context.createMediaStreamSource(stream);
-    const destination = context.createMediaStreamDestination()
-    source.connect(analyser).connect(destination);
 
+    const analyser = context.createAnalyser();
     analyser.fftSize = 2048;
     const data = new Float32Array(analyser.fftSize);
+
+    const destination = context.createMediaStreamDestination();
     
+    source.connect(analyser).connect(destination);
+
     setInterval(() => {
       analyser.getFloatTimeDomainData(data);
       const [pitch, clarity] = findPitch(data, context.sampleRate);
-      if (clarity > 0.8) {
+      if (clarity > 0.9 && pitch > 50) {
         renderNote(findNote(pitch));
       }
     }, 300);
   })
   .catch(err => console.log(err));
+
+drawScale();
