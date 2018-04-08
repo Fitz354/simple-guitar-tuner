37055a9a855b15c8c777b11ccca8bcffc9a5e804
Fitz354 <lozovsky.alx@yandex.ru>
2018-04-08 20:00:50 +0300

    add guitar tunings

--- src/js/tunings.js
+++ src/js/tunings.js
@@ -9,6 +9,51 @@ const standardStrings = [
   { name: 'E', octave: 4 },
 ];
 
+const openCStrings = [
+  { name: 'C', octave: 2 },
+  { name: 'G', octave: 2 },
+  { name: 'C', octave: 3 },
+  { name: 'G', octave: 3 },
+  { name: 'C', octave: 4 },
+  { name: 'E', octave: 4 },
+];
+
+const openDStrings = [
+  { name: 'D', octave: 2 },
+  { name: 'A', octave: 2 },
+  { name: 'D', octave: 3 },
+  { name: 'F#', octave: 3 },
+  { name: 'A', octave: 3 },
+  { name: 'D', octave: 4 },
+];
+
+const openGStrings = [
+  { name: 'D', octave: 2 },
+  { name: 'G', octave: 2 },
+  { name: 'D', octave: 3 },
+  { name: 'G', octave: 3 },
+  { name: 'B', octave: 3 },
+  { name: 'D', octave: 4 },
+];
+
+const dropDStrings = [
+  { name: 'D', octave: 2 },
+  { name: 'A', octave: 2 },
+  { name: 'D', octave: 3 },
+  { name: 'G', octave: 3 },
+  { name: 'B', octave: 3 },
+  { name: 'E', octave: 4 },
+];
+
+const dropCStrings = [
+  { name: 'C', octave: 2 },
+  { name: 'G', octave: 2 },
+  { name: 'C', octave: 3 },
+  { name: 'F', octave: 3 },
+  { name: 'A', octave: 3 },
+  { name: 'D', octave: 4 },
+];
+
 const ukuleleStrings = [
   { name: 'G', octave: 4 },
   { name: 'C', octave: 4 },
@@ -54,6 +99,36 @@ export const standard = {
   name: 'Standard',
   strings: standardStrings,
   parse: getParser(standardStrings),
+};
+
+export const openC = {
+  name: 'Open C',
+  strings: openCStrings,
+  parse: getParser(openCStrings),
+};
+
+export const openD = {
+  name: 'Open D',
+  strings: openDStrings,
+  parse: getParser(openDStrings),
+};
+
+export const openG = {
+  name: 'Open G',
+  strings: openGStrings,
+  parse: getParser(openGStrings),
+};
+
+export const dropD = {
+  name: 'Drop D',
+  strings: dropDStrings,
+  parse: getParser(dropDStrings),
+};
+
+export const dropC = {
+  name: 'Drop C',
+  strings: dropCStrings,
+  parse: getParser(dropCStrings),
 };
 
 export const ukulele = {
