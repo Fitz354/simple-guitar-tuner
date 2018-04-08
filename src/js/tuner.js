7b784a02348c502fb449f7da285605d4aa9b9c8f
Fitz354 <lozovsky.alx@yandex.ru>
2018-04-08 19:21:39 +0300

    set default tuning to standard

--- src/js/tuner.js
+++ src/js/tuner.js
@@ -2,28 +2,33 @@ import { findPitch } from 'pitchy';
 import * as tunings from './tunings';
 import render from './render';
 
-let activeTuning = tunings.chromatic;
-
 const tuningSelectElement = document.querySelector('.tunings-list');
 const stringsListElement = document.querySelector('.strings');
 
-tuningSelectElement.innerHTML =
-  Object.keys(tunings).map(key => `<option value=${key}>${tunings[key].name}</option>`).join('');
+const defaultTuning = 'standard';
+let activeTuning = tunings[defaultTuning];
 
-tuningSelectElement.addEventListener('change', () => {
+tuningSelectElement.innerHTML = Object.keys(tunings)
+  .map(key => `<option value=${key} ${key === defaultTuning && 'selected'}>${tunings[key].name}</option>`)
+  .join('');
+
+const handleTuningChange = () => {
   activeTuning = tunings[tuningSelectElement.value];
   if (!activeTuning.strings) {
     stringsListElement.innerHTML = '';
     return;
   }
 
-  stringsListElement.innerHTML = activeTuning.strings.map(({ name, octave }) =>
-    `<div class="strings-item" data-note="${name + octave}">
-      <div class="string-lightbulb"></div>
-      <span class="string-name">${name + octave}</span>
-    </div>`).join('');
-});
-
+  stringsListElement.innerHTML = activeTuning.strings
+    .map(({ name, octave }) =>
+      `<div class="strings-item" data-note="${name + octave}">
+        <div class="string-lightbulb"></div>
+        <span class="string-name">${name + octave}</span>
+      </div>`)
+    .join('');
+};
+handleTuningChange();
+tuningSelectElement.addEventListener('change', handleTuningChange);
 render({ cents: -50 });
 
 window.navigator.mediaDevices.getUserMedia({ audio: true })
