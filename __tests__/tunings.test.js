ca389e7015c127dd9a30593830cc78a333f54f8b
Fitz354 <lozovsky.alx@yandex.ru>
2018-05-09 18:32:35 +0300

    fix tests

--- __tests__/tunings.test.js
+++ __tests__/tunings.test.js
@@ -1,20 +1,17 @@
 const tunings = require('../src/js/tunings');
 
 it('chromatic', () => {
-  const { parse } = tunings.chromatic;
-
-  expect(parse(27.5)).toEqual({ name: 'A', octave: 0, cents: 0 });
-  expect(parse(150)).toEqual({ name: 'D', octave: 3, cents: 37 });
+  expect(tunings.chromatic.parse(27.5)).toEqual({ name: 'A', octave: 0, cents: 0 });
+  expect(tunings.chromatic.parse(150)).toEqual({ name: 'D', octave: 3, cents: 37 });
 });
 
 it('standard', () => {
-  const { parse } = tunings.standard;
-  expect(parse(20)).toBeFalsy();
-  expect(parse(27.5)).toEqual({ name: 'E', octave: 2, cents: -50 });
-  expect(parse(82.41)).toEqual({ name: 'E', octave: 2, cents: 0 });
-  expect(parse(83)).toEqual({ name: 'E', octave: 2, cents: 12 });
-  expect(parse(85)).toEqual({ name: 'E', octave: 2, cents: 50 });
-  expect(parse(100)).toEqual({ name: 'A', octave: 2, cents: -50 });
-  expect(parse(110)).toEqual({ name: 'A', octave: 2, cents: 0 });
-  expect(parse(350)).toEqual({ name: 'E', octave: 4, cents: 50 });
+  expect(tunings.standard.parse(20)).toBeFalsy();
+  expect(tunings.standard.parse(27.5)).toEqual({ name: 'E', octave: 2, cents: -50 });
+  expect(tunings.standard.parse(82.41)).toEqual({ name: 'E', octave: 2, cents: 0 });
+  expect(tunings.standard.parse(83)).toEqual({ name: 'E', octave: 2, cents: 12 });
+  expect(tunings.standard.parse(85)).toEqual({ name: 'E', octave: 2, cents: 50 });
+  expect(tunings.standard.parse(100)).toEqual({ name: 'A', octave: 2, cents: -50 });
+  expect(tunings.standard.parse(110)).toEqual({ name: 'A', octave: 2, cents: 0 });
+  expect(tunings.standard.parse(350)).toEqual({ name: 'E', octave: 4, cents: 50 });
 });
